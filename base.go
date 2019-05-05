e3e760f861471b23fe22401ce1676c1103ac76b4
Hekmon <edouardhur@gmail.com>
2019-05-05 16:36:21 +0200

    base done

---
+++ base.go
@@ -0,0 +1,124 @@
+package plexwebhook
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"strconv"
+)
+
+// Payload represents the base structure for all plex webhooks
+type Payload struct {
+	// Base
+	Rating  *int      // only present for Event == EventTypeRate
+	Event   EventType `json:"event"`
+	User    bool      `json:"user"`
+	Owner   bool      `json:"owner"`
+	Account Account   `json:"Account"`
+	Server  Server    `json:"Server"`
+	Player  Player    `json:"Player"`
+	// Metadata
+}
+
+// UnmarshalJSON allows to convert json values to Go types
+func (p *Payload) UnmarshalJSON(data []byte) (err error) {
+	type Shadow Payload
+	tmp := struct {
+		Rating string `json:"rating"`
+		*Shadow
+	}{
+		Shadow: (*Shadow)(p),
+	}
+	if tmp.Event == EventTypeRate {
+		var rating int
+		if rating, err = strconv.Atoi(tmp.Rating); err != nil {
+			err = fmt.Errorf("can't convert rating as int: %v", err)
+			return
+		}
+		p.Rating = &rating
+	}
+	return
+}
+
+/*
+	Event type
+*/
+
+// EventType represent the webhook event type
+type EventType string
+
+const (
+	// EventTypePause represents the play event
+	EventTypePause EventType = "media.pause"
+	// EventTypePlay represents the play event
+	EventTypePlay EventType = "media.play"
+	// EventTypeRate represents the rate event
+	EventTypeRate EventType = "media.rate"
+	// EventTypeResume represents the resume event
+	EventTypeResume EventType = "media.resume"
+	// EventTypeScrobble represents the scrobble event
+	EventTypeScrobble EventType = "media.scrobble"
+	// EventTypeStop represents the stop event
+	EventTypeStop EventType = "media.stop"
+)
+
+/*
+	Account
+*/
+
+// Account represent the account which has generated the wehhook launch on the server
+type Account struct {
+	ID    int `json:"id"` // server relative id (owner is 1)
+	Thumb *url.URL
+	Title string `json:"title"` // username
+}
+
+// UnmarshalJSON allows to convert json values to Go types
+func (a *Account) UnmarshalJSON(data []byte) (err error) {
+	type Shadow Account
+	tmp := struct {
+		Thumb string `json:"thumb"`
+		*Shadow
+	}{
+		Shadow: (*Shadow)(a),
+	}
+	if a.Thumb, err = url.Parse(tmp.Thumb); err != nil {
+		err = fmt.Errorf("can't parse account thumb as URL: %v", err)
+	}
+	return
+}
+
+/*
+	Server
+*/
+
+// Server holds informations about the server which generated the webhook
+type Server struct {
+	Title string `json:"title"`
+	UUID  string `json:"uuid"`
+}
+
+/*
+	Player
+*/
+
+// Player holds informations about the user's player
+type Player struct {
+	Local         bool `json:"local"`
+	PublicAddress net.IP
+	Title         string `json:"title"`
+	UUID          string `json:"uuid"`
+}
+
+// UnmarshalJSON allows to convert json values to Go types
+func (p *Player) UnmarshalJSON(data []byte) (err error) {
+	type Shadow Player
+	tmp := struct {
+		PublicAddress string `json:"publicAddress"`
+		*Shadow
+	}{
+		Shadow: (*Shadow)(p),
+	}
+	p.PublicAddress = net.ParseIP(tmp.PublicAddress)
+	return
+}
